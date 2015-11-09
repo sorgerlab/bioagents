efb0622ea1b8e1bae414538a15ac881587064000
Ben Gyori <ben.gyori@gmail.com>
2015-11-09 16:03:22 -0500

    Add simulation comparison upon model perturbation to MEA

--- mea.py
+++ mea.py
@@ -41,6 +41,40 @@ class MEA:
         # TODO: how do we know that we are looking for an active species?
         return monomer.name + '_act'
 
+    def compare_auc(self, ts, y_ref, y_new):
+        '''
+        Return the ratio on the are under two simulation trajectories,
+        y_new and y_rer
+        '''
+        dt = numpy.diff(ts)
+        a_ref = numpy.dot(dt, 0.5*(y_ref[1:] + y_ref[:-1]))
+        a_new = numpy.dot(dt, 0.5*(y_new[1:] + y_new[:-1]))
+        return a_new / a_ref
+
+    def compare_add(self, model, agent_add, agent_target):
+        '''
+        Compare model simulation target with or without adding a given agent.
+        '''
+        # Get the monomer whose addition is of interest
+        monomer = self.get_monomer(model, agent_add)
+        # Get the name of the initial conditions for the monomer
+        # TODO: how do we know that the name is always constructed as below?
+        init_cond_name = agent_add.name + '_0'
+        init_orig = model.parameters[init_cond_name].value
+        # Simulate without the monomer
+        model.parameters[init_cond_name].value = 0
+        yobs_target_noadd = self.simulate_model(model, agent_target)
+        # Simulate with the monomer
+        # TODO: where does this value come from?
+        model.parameters[init_cond_name].value = 100
+        yobs_target_add = self.simulate_model(model, agent_target)
+        # Restore the original initial condition value
+        model.parameters[init_cond_name].value = init_orig
+        # TODO: this should be obtained from simulate_model
+        ts = numpy.linspace(0, 100, 100)
+        auc_ratio = self.compare_auc(ts, yobs_target_noadd, yobs_target_add)
+        return auc_ratio
+        
     def simulate_model(self, model, agent_target):
         '''
         Simulate a model and return the observed dynamics of 

4c4564c7e00adeb56f18a192f9544145d8512232
Ben Gyori <ben.gyori@gmail.com>
2015-11-11 16:45:44 -0500

    Add tests for cbio client

---
+++ test/cbio_client_test.py
@@ -0,0 +1,19 @@
+from bioagents import cbio_client
+
+def test_get_cancer_studies():
+    study_ids = cbio_client.get_cancer_studies('pancreatic')
+    assert(len(study_ids) > 0)
+    assert('paad_tcga' in study_ids)
+
+def test_get_cancer_types():
+    type_ids = cbio_client.get_cancer_types('lung')
+    assert(len(type_ids) > 0)
+
+def test_get_genetic_profiles():
+    genetic_profiles = cbio_client.get_genetic_profiles('paad_icgc', 'mutation')
+    assert(len(genetic_profiles) > 0)
+
+def test_get_num_sequenced():
+    num_case = cbio_client.get_num_sequenced('paad_tcga')
+    assert(num_case > 0)
+

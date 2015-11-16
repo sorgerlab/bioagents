1f65a11b840e9f8c5c55ab904f67cc37d97ea8be
Ben Gyori <ben.gyori@gmail.com>
2015-11-16 16:22:20 -0500

    Update message format for find-treatment task in DTDA

--- dtda_module.py
+++ dtda_module.py
@@ -19,9 +19,9 @@ class DTDA_Module(trips_module.TripsModule):
     def __init__(self, argv):
         # Call the constructor of TripsModule
         super(DTDA_Module, self).__init__(argv)
-        # Instantiate a singleton DTDA agent
-        self.tasks = ['is-drug-target', 'find-target-drug', 
-                    'find-disease-targets', 'find-treatment']
+        self.tasks = {'ONT::PERFORM': ['ONT::IS-DRUG-TARGET', 
+                    'ONT::FIND-TARGET-DRUG', 'ONT::FIND-DISEASE-TARGETS', 
+                    'ONT::FIND-TREATMENT']}
 
     def init(self):
         '''
@@ -29,11 +29,14 @@ class DTDA_Module(trips_module.TripsModule):
         '''
         super(DTDA_Module, self).init()
         # Send subscribe messages
-        for task in self.tasks:
-            task_str = 'ONT::%s' % task.upper()
-            msg_txt = '(subscribe :content (request &key :content (%s . *)))' % task_str
-            self.send(KQMLPerformative.fromString(msg_txt))
+        for task, subtasks in self.tasks.iteritems():
+            for subtask in subtasks:
+                msg_txt = '(subscribe :content (request &key :content ' +\
+                    '(%s &key :content (%s . *))))' % (task, subtask)
+                self.send(KQMLPerformative.fromString(msg_txt))
+        # Instantiate a singleton DTDA agent
         self.dtda = DTDA()
+        # Send ready message
         self.ready()
 
     def receive_request(self, msg, content):
@@ -43,20 +46,22 @@ class DTDA_Module(trips_module.TripsModule):
         "tell" message is then sent back.
         '''
         content_list = cast(KQMLList, content)
-        task = content_list.get(0).toString().upper()
-        sender = msg.getParameter(":sender")
-        if task == 'ONT::IS-DRUG-TARGET':
-            reply_content = self.respond_is_drug_target(content_list)
-        elif task == 'ONT::FIND-TARGET-DRUG':
-            reply_content = self.respond_find_target_drug(content_list)
-        elif task == 'ONT::FIND-DISEASE-TARGETS':
-            reply_content = self.respond_find_disease_targets(content_list)
-        elif task == 'ONT::FIND-TREATMENT':
-            reply_content = self.respond_find_treatment(content_list)
-        else:
-            self.error_reply(msg, 'unknown request task ' + task)
-            return
-        
+        task_str = content_list.get(0).toString().upper()
+        if task_str == 'ONT::PERFORM':
+            subtask = cast(KQMLList,content_list.getKeywordArg(':content'))
+            subtask_str = subtask.get(0).toString().upper()
+            if subtask_str == 'ONT::IS-DRUG-TARGET':
+                reply_content = self.respond_is_drug_target(content_list)
+            elif subtask_str == 'ONT::FIND-TARGET-DRUG':
+                reply_content = self.respond_find_target_drug(content_list)
+            elif subtask_str == 'ONT::FIND-DISEASE-TARGETS':
+                reply_content = self.respond_find_disease_targets(content_list)
+            elif subtask_str == 'ONT::FIND-TREATMENT':
+                reply_content = self.respond_find_treatment(subtask)
+            else:
+                self.error_reply(msg, 'unknown request task ' + task)
+                return
+
         reply_msg = KQMLPerformative('reply')
         reply_msg.setParameter(':content', cast(KQMLObject, reply_content))
         self.reply(msg, reply_msg)
@@ -110,11 +115,11 @@ class DTDA_Module(trips_module.TripsModule):
         disease_terms = disease_str[1:-1].split(' ')
         disease = disease_terms[0]
         disease_type = disease_terms[1]
-        if disease.upper() != 'ONT::CANCER':
+        if disease.upper() != 'W::CANCER':
             print 'problem with disease name'
             reply_content.add('')
             return reply_content
-        disease_type_filter = disease_type[5:].lower()
+        disease_type_filter = disease_type[3:].lower()
         
         # First, look for possible disease targets
         mutation_stats = self.dtda.get_mutation_statistics(disease_type_filter, 'missense')
@@ -128,8 +133,23 @@ class DTDA_Module(trips_module.TripsModule):
         top_mutation = mutations_sorted[0]
         mut_protein = top_mutation[0]
         mut_percent = int(top_mutation[1][0]*100.0)
+        
+        mut_response = KQMLList.fromString('(ONT::TELL :content ' +\
+            '(ONT::PROPORTION :refset (ONT::CANCER-PATIENT) ' +\
+            ':quan (ONT::PERCENT %d) ' % mut_percent +\
+            ':content (ONT::MUTATION :affected ONT::%s)))' % mut_protein)
 
-        reply_content = KQMLList.fromString('(ONT::PROPORTION :REFSET (ONT::CANCER-PATIENT) :QUAN (ONT::PERCENT %d) :CONTENT (ONT::MUTATION :AFFECTED ONT::%s))' % (mut_percent, mut_protein))
+        # Try to find a drug targeting KRAS
+        drugs = self.dtda.find_target_drug(mut_protein)
+        #import ipdb; ipdb.set_trace()
+        if not drugs:
+            drug_response = KQMLList.fromString('(ONT::TELL :content (ONT::DONT-KNOW ' +\
+                ':content (ONT::A X1 :instance-of ONT::DRUG)))')
+        else:
+            drug_response = KQMLList.fromString('(ONT::TELL :content ())')
+        reply_content.add(KQMLList(mut_response))
+        reply_content.add(KQMLList(drug_response))
+
         return reply_content
 
 if __name__ == "__main__":

e0ed68279f3901df1289a7dd9eb2f2706228d633
Ben Gyori <ben.gyori@gmail.com>
2016-03-10 20:46:21 -0500

    Add DTDA integration test file and allow file argument in tester

--- integration/simple_test_module.py
+++ integration/simple_test_module.py
@@ -12,37 +12,19 @@ KQMLPerformative = autoclass('TRIPS.KQML.KQMLPerformative')
 KQMLList = autoclass('TRIPS.KQML.KQMLList')
 KQMLObject = autoclass('TRIPS.KQML.KQMLObject')
 
-class FIFO(object):
-    def __init__(self, lst=None):
-        if lst is None:
-            self.lst = []
-        else:
-            self.lst = lst
-
-    def pop(self):
-        return self.lst.pop()
-
-    def push(self, e):
-        self.lst = [e] + self.lst
-
-    def is_empty(self):
-        if not self.lst:
-            return True
-        return False
-
 class TestModule(TripsModule):
     '''
     The Test module is a TRIPS module built to run unit test. It will
     ts role is to receive and decode messages and send responses from
     and to other agents in the system.
     '''
-    def __init__(self, argv):
+    def __init__(self, argv, file_in):
         # Call the constructor of TripsModule
-        super(TestModule, self).__init__(argv)
+        super(TestModule, self).__init__(argv[1:])
         self.expected = FIFO()
         self.sent = FIFO()
         # TODO:make this an input argument
-        self.test_file = 'integration/test.in'
+        self.test_file = file_in
         self.msg_counter = 1
 
     def init(self):
@@ -90,6 +72,24 @@ class TestModule(TripsModule):
         if self.expected.is_empty():
             sys.exit(0)
 
+class FIFO(object):
+    def __init__(self, lst=None):
+        if lst is None:
+            self.lst = []
+        else:
+            self.lst = lst
+
+    def pop(self):
+        return self.lst.pop()
+
+    def push(self, e):
+        self.lst = [e] + self.lst
+
+    def is_empty(self):
+        if not self.lst:
+            return True
+        return False
+
 if __name__ == "__main__":
-    dm = TestModule(['-name', 'Test'] + sys.argv[1:])
+    dm = TestModule(['-name', 'Test'] + sys.argv[2:], sys.argv[1])
     dm.run()

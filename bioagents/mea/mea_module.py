4681d5dd41f07ff2d5bae84e3981e1450884aa38
Ben Gyori <ben.gyori@gmail.com>
2016-06-07 14:15:57 -0400

    Handle simulate model errors in MEA

--- bioagents/mea/mea_module.py
+++ bioagents/mea/mea_module.py
@@ -11,11 +11,6 @@ from bioagents.trips.kqml_list import KQMLList
 
 logger = logging.getLogger('MEA')
 
-class InvalidModelException(Exception):
-    def __init__(self, *args, **kwargs):
-            Exception.__init__(self, *args, **kwargs)
-
-
 class MEA_Module(trips_module.TripsModule):
     def __init__(self, argv):
         super(MEA_Module, self).__init__(argv)
@@ -49,9 +44,12 @@ class MEA_Module(trips_module.TripsModule):
         content_list = content
         task_str = content_list[0].to_string().upper()
         if task_str == 'SIMULATE-MODEL':
-            reply_content = self.respond_simulate_model(content_list)
+            try:
+                reply_content = self.respond_simulate_model(content_list)
+            except Exception as e:
+                self.error_reply(msg, 'Error in performing simulation task.')
         else:
-            self.error_reply(msg, 'unknown request task ' + task_str)
+            self.error_reply(msg, 'Unknown request task ' + task_str)
             return
 
         reply_msg = KQMLPerformative('reply')
@@ -63,14 +61,13 @@ class MEA_Module(trips_module.TripsModule):
         Response content to simulate-model request
         '''
         model_str = content_list.get_keyword_arg(':model')
-        if model_str is not None:
+        try:
             model_str = model_str.to_string()
-            try:
-                model = self.decode_model(model_str[1:-1])
-            except InvalidModelException:
-                reply_content =\
-                    KQMLList.from_string('(FAILURE :reason INVALID_MODEL)')
-                return reply_content
+            model = self.decode_model(model_str[1:-1])
+        except InvalidModelDescriptionError:
+            reply_content =\
+                KQMLList.from_string('(FAILURE :reason INVALID_MODEL)')
+            return reply_content
         target_entity = content_list.get_keyword_arg(':target_entity')
         if target_entity is not None:
             target_entity = target_entity.to_string()[1:-1]
@@ -121,20 +118,20 @@ class MEA_Module(trips_module.TripsModule):
 
     @staticmethod
     def decode_model(model_enc_str):
-        try:
-            model_str = base64.b64decode(model_enc_str)
-        except:
-            raise InvalidModelException
+        model_str = model_enc_str
         model = MEA_Module.model_from_string(model_str)
         return model
 
     @staticmethod
     def model_from_string(model_str):
-        with open('tmp_model.py', 'wt') as fh:
-            fh.write(model_str)
-        # TODO: executing the model string is not safe
-        # we should do this through a safer method
-        exec model_str
+        try:
+            with open('tmp_model.py', 'wt') as fh:
+                fh.write(model_str)
+            # TODO: executing the model string is not safe
+            # we should do this through a safer method
+            exec model_str
+        except Exception as e:
+            raise InvalidModelDescriptionError
         logger.debug('\n\n')
         logger.debug('------BEGIN received model------')
         logger.debug('%s' % model_str)
@@ -146,6 +143,9 @@ class MEA_Module(trips_module.TripsModule):
         logger.debug('-------END received model------')
         logger.debug('\n\n')
         return model
+
+class InvalidModelDescriptionError(Exception):
+    pass
 
 if __name__ == "__main__":
     m = MEA_Module(['-name', 'MEA'] + sys.argv[1:]).run()

e7c62838aabdda98b8f6aca38f45571d8ed05377
Ben Gyori <ben.gyori@gmail.com>
2016-06-28 00:22:23 -0400

    Add new tests

--- tests/tra_test.py
+++ tests/tra_test.py
@@ -28,13 +28,19 @@ def test_get_time_interval_badunit():
 
 def test_get_molecular_entity():
     me = KQMLList.from_string('(:description "%s")' % ekb_complex)
-    print me
     ent = tra_module.get_molecular_entity(me)
+
+def test_get_molecular_condition():
+    lst = KQMLList.from_string('(:type "decrease" :quantity (:type "total" ' +\
+                               ':entity (:description "%s")))' % ekb_braf)
+    mc = tra_module.get_molecular_condition(lst)
 
 def test_get_temporal_pattern():
     pattern_msg = '(:type "transient" :entities ((:description ' + \
                     '"%s")))' % ekb_complex
     lst = KQMLList.from_string(pattern_msg)
     ent = tra_module.get_temporal_pattern(lst)
+
+ekb_braf = '<ekb><TERM dbid=\\"UP:P15056|HGNC:1097\\" id=\\"V34744\\"><type>ONT::GENE</type><name>BRAF</name><text>BRAF</text></TERM></ekb>'
 
 ekb_complex = '<ekb><TERM id=\\"V34770\\"><type>ONT::MACROMOLECULAR-COMPLEX</type><components><component id=\\"V34744\\"/><component id=\\"V34752\\"/></components><text normalization=\\"\\">The BRAF-KRAS complex</text></TERM> <TERM dbid=\\"UP:P15056|HGNC:1097\\" id=\\"V34744\\"><type>ONT::GENE</type><name>BRAF</name><text>The BRAF-KRAS complex</text></TERM> <TERM dbid=\\"UP:P79800|HGNC:6407|UP:Q5EFX7|UP:O42277|UP:P01116|UP:Q05147|XFAM:PF00071|UP:Q9YH38\\" id=\\"V34752\\"><type>ONT::GENE-PROTEIN</type><name>KRAS</name><text>KRAS</text></TERM></ekb>'

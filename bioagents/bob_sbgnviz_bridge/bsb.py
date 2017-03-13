c42a1dcd3cb4b4b0fe153ab6e40beb080540922a
Ben Gyori <ben.gyori@gmail.com>
2017-03-13 17:22:15 -0400

    Refactor BSB as a class

--- bioagents/bob_sbgnviz_bridge/bsb.py
+++ bioagents/bob_sbgnviz_bridge/bsb.py
@@ -3,99 +3,112 @@ import time
 import json
 import random
 import select
-from socketIO_client import SocketIO
 import socket
+import logging
+from socketIO_client import SocketIO
 
 from kqml import *
 
-user_name = 'BOB'
-_id_symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-def generate_id(length=32, symbols=_id_symbols):
-    symbol_gen = (symbols[random.randrange(0, len(symbols))]
-                  for i in range(length))
-    return ''.join(symbol_gen)
-
-user_id = generate_id()
-
-def on_message(data):
-    print(data)
-
-def on_user_list(data):
-    print(data)
-
-def bob_startup(sock):
-    msg = '(register :name bsb)'
-    sock.sendall(msg)
-    msg = '(subscribe :content (tell &key :content (spoken . *)))'
-    sock.sendall(msg)
-    msg = '(tell :content (module-status ready))'
-    sock.sendall(msg)
-
-def sbgn_startup(sock, room_id):
-    event = 'subscribeAgent'
-    user_info = {'userName': user_name,
-                 'room': room_id,
-                 'userId': user_id}
-    sock.on('message', on_sbgnviz_message)
-    sock.on('userList', dummy)
-    sock.emit(event, user_info, dummy)
-
-def send_to_bob(msg):
-    socket_b.sendall(msg)
+logger = logging.getLogger('bsb')
 
 def dummy(arg1):
     print(arg1)
 
-def on_sbgnviz_message(data):
-    if not isinstance(data, dict):
-        return
-    comment = data.get('comment')
-    if comment and comment.startswith('bob:'):
-        text = comment[4:].strip()
-        msg = '(tell :content (started-speaking :mode text :uttnum 1 ' + \
-                ':channel Desktop :direction input))'
-        send_to_bob(msg)
-        msg = '(tell :content (stopped-speaking :mode text :uttnum 1 ' + \
-                ':channel Desktop :direction input))'
-        send_to_bob(msg)
-        msg = '(tell :content (word "%s" :uttnum 1 :index 1 ' % text + \
-                ':channel Desktop :direction input))'
-        send_to_bob(msg)
-        msg = '(tell :content (utterance :mode text :uttnum 1 ' + \
-                ':text "%s" ' % text + \
-                ':channel Desktop :direction input))'
-        send_to_bob(msg)
+class BSB(object):
+    def __init__(self, room_id, bob_port=6200, sbgnviz_port=3000):
+        self.user_name = 'BOB'
+        self.room_id = room_id
+        self.bob_port = bob_port
+        self.sbgnviz_port = sbgnviz_port
 
-def on_bob_message(data):
-    msg = {'room': room_id, 'comment': data, 'userName': user_name,
-            'userId': user_id, 'time': 1}
-    socket_s.emit('agentMesage', msg, lambda: None)
+        # Startup sequences
+        self.bob_startup()
+        self.sbgn_startup()
+
+    def start(self):
+        logger.info('Starting...')
+        # Wait for things to happen
+        socks = [self.socket_b, self.socket_s._transport._connection.sock]
+        while True:
+            try:
+                ready_socks, _, _ = select.select(socks, [], [])
+                for sock in ready_socks:
+                    if sock == self.socket_s._transport._connection.sock:
+                        self.socket_s.wait(seconds=0.1)
+                    else:
+                        data, addr = sock.recvfrom(4086)
+                        if data:
+                            self.on_bob_message(data)
+            except KeyboardInterrupt:
+                break
+        self.socket_s.emit('disconnect')
+        self.socket_s.disconnect()
+
+    def bob_startup(self):
+        logger.info('Initializing Bob connection...')
+        self.socket_b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_b.connect(('localhost', self.bob_port))
+        msg = '(register :name bsb)'
+        self.socket_b.sendall(msg)
+        msg = '(subscribe :content (tell &key :content (spoken . *)))'
+        self.socket_b.sendall(msg)
+        msg = '(tell :content (module-status ready))'
+        self.socket_b.sendall(msg)
+
+    def sbgn_startup(self):
+        logger.info('Initializing SBGNViz connection...')
+        _id_symbols = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        def generate_id(length=32, symbols=_id_symbols):
+            symbol_gen = (symbols[random.randrange(0, len(symbols))]
+                          for i in range(length))
+            return ''.join(symbol_gen)
+
+        self.user_id = generate_id()
+        # Initialize sockets
+        self.socket_s = SocketIO('localhost', self.sbgnviz_port)
+        event = 'subscribeAgent'
+        user_info = {'userName': self.user_name,
+                     'room': self.room_id,
+                     'userId': self.user_id}
+        self.socket_s.on('message', self.on_sbgnviz_message)
+        self.socket_s.on('userList', dummy)
+        self.socket_s.emit(event, user_info, dummy)
+
+    def on_user_list(self, data):
+        print(data)
+
+    def send_to_bob(self, msg):
+        self.socket_b.sendall(msg)
+
+    def on_sbgnviz_message(self, data):
+        if not isinstance(data, dict):
+            return
+        comment = data.get('comment')
+        if comment and comment.startswith('bob:'):
+            text = comment[4:].strip()
+            msg = '(tell :content (started-speaking :mode text :uttnum 1 ' + \
+                    ':channel Desktop :direction input))'
+            self.send_to_bob(msg)
+            msg = '(tell :content (stopped-speaking :mode text :uttnum 1 ' + \
+                    ':channel Desktop :direction input))'
+            self.send_to_bob(msg)
+            msg = '(tell :content (word "%s" :uttnum 1 :index 1 ' % text + \
+                    ':channel Desktop :direction input))'
+            self.send_to_bob(msg)
+            msg = '(tell :content (utterance :mode text :uttnum 1 ' + \
+                    ':text "%s" ' % text + \
+                    ':channel Desktop :direction input))'
+            self.send_to_bob(msg)
+
+    def on_bob_message(self, data):
+        msg = {'room': self.room_id,
+               'comment': data,
+               'userName': self.user_name,
+               'userId': self.user_id,
+               'time': 1}
+        self.socket_s.emit('agentMesage', msg, lambda: None)
 
 if __name__ == '__main__':
     room_id = sys.argv[1]
-
-    # Initialize sockets
-    socket_s = SocketIO('localhost', '3000')
-    socket_b = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_b.connect(('localhost', 6200))
-
-    # Startup sequences
-    bob_startup(socket_b)
-    sbgn_startup(socket_s, room_id)
-
-    # Wait for things to happen
-    socks = [socket_b, socket_s._transport._connection.sock]
-    while True:
-        try:
-            ready_socks, _, _ = select.select(socks, [], [])
-            for sock in ready_socks:
-                if sock == socket_s._transport._connection.sock:
-                    socket_s.wait(seconds=0.1)
-                else:
-                    data, addr = sock.recvfrom(4086)
-                    if data:
-                        on_bob_message(data)
-        except KeyboardInterrupt:
-            break
-    socket_s.emit('disconnect')
-    socket_s.disconnect()
+    bsb = BSB(room_id)
+    bsb.start()

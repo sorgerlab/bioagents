2156b0ed5d2348da98aebc7ae3dd02b9426624fb
Ben Gyori <ben.gyori@gmail.com>
2017-04-18 21:59:15 -0400

    Update DTDA with new KQML messaging

--- bioagents/dtda/dtda_module.py
+++ bioagents/dtda/dtda_module.py
@@ -12,11 +12,9 @@ from dtda import DTDA, Disease, \
 
 # TODO: standardize dash/underscore
 class DTDA_Module(KQMLModule):
-    '''
-    The DTDA module is a TRIPS module built around the DTDA agent.
+    """The DTDA module is a TRIPS module built around the DTDA agent.
     Its role is to receive and decode messages and send responses from and
-    to other agents in the system.
-    '''
+    to other agents in the system."""
     def __init__(self, argv):
         # Call the constructor of TripsModule
         super(DTDA_Module, self).__init__(argv)
@@ -34,11 +32,9 @@ class DTDA_Module(KQMLModule):
         super(DTDA_Module, self).start()
 
     def receive_request(self, msg, content):
-        '''
-        If a "request" message is received, decode the task and the content
+        """If a "request" message is received, decode the task and the content
         and call the appropriate function to prepare the response. A reply
-        message is then sent back.
-        '''
+        message is then sent back."""
         content_list = content
         task_str = content_list[0].to_string().upper()
         if task_str == 'IS-DRUG-TARGET':
@@ -58,7 +54,7 @@ class DTDA_Module(KQMLModule):
             return
 
         reply_msg = KQMLPerformative('reply')
-        reply_msg.set_parameter(':content', reply_content)
+        reply_msg.set('content', reply_content)
         self.reply(msg, reply_msg)
 
     def respond_dont_know(self, msg, content_string):
@@ -66,23 +62,20 @@ class DTDA_Module(KQMLModule):
             content_string
         resp_list = KQMLList.from_string(resp)
         reply_msg = KQMLPerformative('reply')
-        reply_msg.set_parameter(':content', resp_list)
+        reply_msg.set('content', resp_list)
         self.reply(msg, reply_msg)
 
     def respond_is_drug_target(self, content_list):
-        '''
-        Response content to is-drug-target request
-        '''
-        drug_arg = content_list.get_keyword_arg(':drug')
-        drug = drug_arg[0].to_string()
-        target_arg = content_list.get_keyword_arg(':target')
+        """Response content to is-drug-target request."""
+        drug = content_list.gets('drug')
+        target_arg = content_list.gets('target')
         target = self._get_target(target_arg)
         target_name = target.name
-        reply_content = KQMLList()
         try:
             is_target = self.dtda.is_nominal_drug_target(drug, target_name)
         except DrugNotFoundException:
-            reply_content.add('FAILURE :reason DRUG_NOT_FOUND')
+            reply_content = \
+                KQMLList.from_string('(FAILURE :reason DRUG_NOT_FOUND)')
             return reply_content
         status = 'SUCCESS'
         if is_target:
@@ -91,14 +84,12 @@ class DTDA_Module(KQMLModule):
             is_target_str = 'FALSE'
         msg_str = '%s :is-target %s' %\
                   (status, is_target_str)
-        reply_content.add(msg_str)
+        reply_content = KQMLList.from_string(msg_str)
         return reply_content
 
     def respond_find_target_drug(self, content_list):
-        '''
-        Response content to find-target-drug request
-        '''
-        target_arg = content_list.get_keyword_arg(':target')
+        """Response content to find-target-drug request."""
+        target_arg = content_list.gets('target')
         target = self._get_target(target_arg)
         target_name = target.name
         drug_names, chebi_ids = self.dtda.find_target_drugs(target_name)
@@ -113,10 +104,8 @@ class DTDA_Module(KQMLModule):
         return reply_content
 
     def respond_find_disease_targets(self, content_list):
-        '''
-        Response content to find-disease-targets request
-        '''
-        disease_arg = content_list.get_keyword_arg(':disease')
+        """Response content to find-disease-targets request."""
+        disease_arg = content_list.gets('disease')
         try:
             disease = self.get_disease(disease_arg)
         except Exception as e:
@@ -153,12 +142,9 @@ class DTDA_Module(KQMLModule):
         return reply_content
 
     def respond_find_treatment(self, content_list):
-        '''
-        Response content to find-treatment request
-        '''
-        reply_content = KQMLList()
+        """Response content to find-treatment request."""
         #TODO: eliminate code duplication here
-        disease_arg = content_list.get_keyword_arg(':disease')
+        disease_arg = content_list.gets('disease')
         try:
             disease = self.get_disease(disease_arg)
         except Exception as e:
@@ -168,7 +154,8 @@ class DTDA_Module(KQMLModule):
             return reply_content
 
         if disease.disease_type != 'cancer':
-            reply_content.add('FAILURE :reason DISEASE_NOT_FOUND')
+            msg_str = '(FAILURE :reason DISEASE_NOT_FOUND)'
+            reply_content = KQMLList.from_string(msg_str)
             return reply_content
 
         logger.debug('Disease: %s' % disease.name)
@@ -177,17 +164,18 @@ class DTDA_Module(KQMLModule):
             mut_protein, mut_percent = \
                 self.dtda.get_top_mutation(disease.name)
         except DiseaseNotFoundException:
-            reply_content.add('FAILURE :reason DISEASE_NOT_FOUND')
+            msg_str = '(FAILURE :reason DISEASE_NOT_FOUND)'
+            reply_content = KQMLList.from_string(msg_str)
             return reply_content
 
         # TODO: get functional effect from actual mutations
         # TODO: add list of actual mutations to response
         # TODO: get fraction not percentage from DTDA
-        reply_content.add(KQMLList.from_string(
+        reply_content = KQMLList.from_string(
                 '(SUCCESS ' +
                 ':protein (:name %s :hgnc %s) ' % (mut_protein, mut_protein) +
                 ':prevalence %.2f ' % (mut_percent/100.0) +
-                ':functional-effect ACTIVE)'))
+                ':functional-effect ACTIVE)')
 
         # Try to find a drug
         drug_names, chebi_ids = self.dtda.find_target_drugs(mut_protein)
@@ -197,23 +185,20 @@ class DTDA_Module(KQMLModule):
                 drug_list_str += '(:name %s) ' % dn.encode('ascii', 'ignore')
             else:
                 drug_list_str += '(:name %s :chebi_id %s) ' % (dn, ci)
-        reply_content.add(KQMLList.from_string(
-            '(SUCCESS :drugs (' + drug_list_str + '))'))
+        reply_content = KQMLList.from_string(
+            '(SUCCESS :drugs (' + drug_list_str + '))')
 
         return reply_content
 
-    def _get_target(self, target_arg):
-        target_str = str(target_arg)
-        target_str = self.decode_description('<ekb>' + target_str + '</ekb>')
+    def _get_target(self, target_str):
+        target_str = '<ekb>' + target_str + '</ekb>'
         tp = TripsProcessor(target_str)
         terms = tp.tree.findall('TERM')
         term_id = terms[0].attrib['id']
         agent = tp._get_agent_by_id(term_id, None)
         return agent
 
-    def get_disease(self, disease_arg):
-        disease_str = str(disease_arg)
-        disease_str = self.decode_description(disease_str)
+    def get_disease(self, disease_str):
         term = ET.fromstring(disease_str)
         disease_type = term.find('type').text
         if disease_type.startswith('ONT::'):
@@ -225,25 +210,6 @@ class DTDA_Module(KQMLModule):
         dbid_dict = {k: v for k, v in [d.split(':') for d in dbids]}
         disease = Disease(disease_type, dbname, dbid_dict)
         return disease
-
-    @staticmethod
-    def get_single_argument(arg):
-        if arg is None:
-            return None
-        arg_str = arg.to_string()
-        if arg_str[0] == '(' and arg_str[-1] == ')':
-             arg_str = arg_str[1:-1]
-        arg_str = arg_str.lower()
-        return arg_str
-
-    @staticmethod
-    def decode_description(descr):
-        if descr[0] == '"':
-            descr = descr[1:]
-        if descr[-1] == '"':
-            descr = descr[:-1]
-        descr = descr.replace('\\"', '"')
-        return descr
 
 if __name__ == "__main__":
     DTDA_Module(['-name', 'DTDA'] + sys.argv[1:])

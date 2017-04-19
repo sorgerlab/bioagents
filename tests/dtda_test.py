375aef45f5719cbfb8b2562fed2b7b93eb90a969
Ben Gyori <ben.gyori@gmail.com>
2017-04-18 22:32:29 -0400

    Update DTDA tests

--- tests/dtda_test.py
+++ tests/dtda_test.py
@@ -1,4 +1,4 @@
-from bioagents.trips.kqml_list import KQMLList
+from kqml import KQMLList
 from bioagents.dtda import DTDA
 from bioagents.dtda import DTDA_Module
 
@@ -7,11 +7,3 @@ def test_mutation_statistics():
     mutation_dict = \
         d.get_mutation_statistics('pancreatic carcinoma', 'missense')
     assert(mutation_dict['KRAS'] > 0)
-
-def test_get_disease():
-    disease_str = '(CANCER :DBNAME "pancreatic carcinoma" :DBID "EFO:0002618")'
-    kl = KQMLList.from_string(disease_str)
-    disease = DTDA_Module.get_disease(kl)
-    assert(disease.disease_type == 'cancer')
-    assert(disease.name == 'pancreatic carcinoma')
-    assert(disease.db_refs == {'EFO': '0002618'})

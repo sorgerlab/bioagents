9a24bce04010584ecbd33a5ac84baa43bb82e61f
pagreene <patrick.anton.greene@gmail.com>
2017-08-18 14:54:17 -0400

    Improve test harness and add coloring feature to test output. Remove deprecated test input files.

--- integration/test_integration.py
+++ integration/test_integration.py
@@ -4,7 +4,7 @@ import logging
 import re
 from subprocess import Popen, PIPE
 from time import sleep
-from os import path, listdir, environ
+from os import path, listdir, environ, remove
 from docutils.io import InputError
 from threading import Thread, Event
 from bioagents.tra.tra_module import TRA_Module
@@ -21,7 +21,23 @@ logger = logging.getLogger('Test')
 
 from kqml import KQMLModule, KQMLPerformative, KQMLList
 
-BIOAGENT_DICT = {
+from difflib import SequenceMatcher
+try:
+    from colorama import Back, Style, Fore
+except ImportError:
+    raise Warning("Will not be able to mark diffs with color.")
+    # Create dummies
+    class DummyColorer(object):
+        def __getattribute__(self, *args, **kwargs):
+            return ""
+    class Back(DummyColorer):
+        pass
+    class Style(DummyColorer):
+        pass
+    class Fore(DummyColorer):
+        pass
+
+BIOAGENT_DICT={
     'tra': TRA_Module,
     'qca': QCA_Module,
     'mra': MRA_Module,
@@ -71,6 +87,8 @@ class Test_Module(KQMLModule):
         # Send ready message
         self.ready()
         self.initialize_tests(file_in)
+
+        self.logger = logging.getLogger(self.name + '_Module')
         return None
 
     def get_perf(self, msg_id, msg_txt):
@@ -89,32 +107,53 @@ class Test_Module(KQMLModule):
             self.sent.push(sm)
             self.expected.push(em)
         self.total_tests = len(self.sent.lst)
-        logger.info('Collected %s test messages from %s' % \
+        self.logger.info('Collected %s test messages from %s' % \
               (self.total_tests, test_file))
         # Send off the first test
         sm = self.sent.pop()
         self.send(self.get_perf(self.msg_counter, sm))
         self.msg_counter += 1
 
+    def color_diff(self, str1, str2):
+        """Show in color the change in a string compaired to another."""
+        sm = SequenceMatcher(None, str2, str1)
+        output= []
+        for opcode, a0, a1, b0, b1 in sm.get_opcodes():
+            if opcode == 'equal':
+                output.append(sm.a[a0:a1])
+            elif opcode == 'insert':
+                output.append(Back.RED + sm.b[b0:b1] + Style.RESET_ALL)
+            elif opcode == 'delete':
+                output.append(Back.GREEN + sm.a[a0:a1] + Style.RESET_ALL)
+            elif opcode == 'replace':
+                output.append(Back.CYAN + sm.a[a0:a1] + Back.RESET + 
+                              Fore.CYAN + sm.b[b0:b1] + Style.RESET_ALL)
+            else:
+                raise RuntimeError, "unexpected opcode"
+        return ''.join(output)
+
+
     def receive_reply(self, msg, content):
         """Handle a "reply" message being received."""
         expected_content = self.expected.pop().strip()
         actual_content = content.__repr__().strip()
-        logger.info('expected: %s' % expected_content)
-        logger.info('actual:   %s' % actual_content)
+        colored_content = self.color_diff(expected_content, actual_content)
+        self.logger.info('expected:  %s' % expected_content)
+        self.logger.info('actual:    %s' % actual_content)
+        self.logger.info('colordiff: %s' % colored_content)
         if expected_content == actual_content:
             self.passed_tests += 1
-            logger.info('PASS')
+            self.logger.info('PASS')
         else:
-            logger.info('FAIL')
-        logger.info('---')
+            self.logger.info('FAIL')
+        self.logger.info('---')
 
         if not self.sent.is_empty():
             sm = self.sent.pop()
             self.send(self.get_perf(self.msg_counter, sm))
             self.msg_counter += 1
         if self.expected.is_empty():
-            logger.info('%d PASSED / %d FAILED' % \
+            self.logger.info('%d PASSED / %d FAILED' % \
                         (self.passed_tests,
                          self.total_tests - self.passed_tests))
             self.dispatcher.shutdown()
@@ -143,28 +182,35 @@ class FIFO(object):
 class Test_Harness(object):
     """The master testing object that will manage the test module and inputs"""
     def __init__(self, inputs):
-        self._find_facilitator()
+        self._cleaning = True
+        clean_opts = {'--nocleanup', '-nc'}.intersection(inputs)
+        if len(clean_opts) > 0:
+            self._cleaning = False
+            [inputs.remove(opt) for opt in clean_opts]
+        self._facilitator = self._find_facilitator()
+
         logger.info("Looking up the available tests.")
-        loc = path.dirname(path.abspath(__file__))
+        self._loc = path.dirname(path.abspath(__file__))
         patt = re.compile(r'(test_(\w+?)\.in)')
-        matches=filter(lambda x: x is not None, map(patt.match, listdir(loc)))
-        self.input_files = {}
-        for m in matches:
-            self.input_files[m.groups()[1]] = m.groups()[0]
+        self._init_state = listdir(self._loc)
+        matches = map(patt.match, self._init_state)
+        self._input_files = {}
+        for m in filter(lambda x: x is not None, matches):
+            self._input_files[m.groups()[1]] = m.groups()[0]
 
         logger.info("Selecting tests to run.")
-        self.run_with = []
+        self._test_list = []
         if 'all' in inputs:
-            self.run_with = self.input_files.keys()
+            self._test_list = self._input_files.keys()
         else:
             for inp in inputs:
-                if inp in self.input_files.keys():
-                    self.run_with.append(inp)
+                if inp in self._input_files.keys():
+                    self._test_list.append(inp)
                 else:
                     raise InputError('Unrecognized bioagent test: %s' % inp)
 
-        self.trips_handle = None
-        self.bioagent_handle = None
+        self._trips_handle = None
+        self._bioagent_handle = None
         return
 
     def _find_facilitator(self):
@@ -173,88 +219,104 @@ class Test_Harness(object):
             home = path.expanduser('~')
             trips_base = path.join(home, 'Workspace', 'cwc-integ',
                                    'trips', 'bob')
-        self.facilitator = path.join(trips_base, 'bin', 'Facilitator')
-        if not path.exists(self.facilitator):
+        facilitator = path.join(trips_base, 'bin', 'Facilitator')
+        if not path.exists(facilitator):
             logger.error('Could not find TRIPS Facilitator at %s' %
-                         self.facilitator)
+                         facilitator)
             sys.exit(1)
+        return facilitator
 
-    def trips_is_running(self):
+    def _trips_is_running(self):
         """Check if the trips process is running"""
-        if self.trips_handle is None:
+        if self._trips_handle is None:
             return False
-        if self.trips_handle.poll() is None:
+        if self._trips_handle.poll() is None:
             return True
         return False
 
-    def start_trips(self):
+    def _start_trips(self):
         """Begin the trips process"""
         logger.info("Starting trips.")
-        if not self.trips_is_running():
+        if not self._trips_is_running():
             with open('init.trips', 'rb') as inp:
-                self.trips_handle = Popen([self.facilitator], stdin=inp)
-            if not self.trips_is_running():
+                self._trips_handle = Popen([self._facilitator], stdin=inp)
+            if not self._trips_is_running():
                 #TODO: Give move feedback by getting stdout from trips.
                 raise TestError('Trips facilitator failed to start.')
             time.sleep(5)
         return
 
-    def run_bioagent_test(self, ba_name):
+    def _run_bioagent_test(self, ba_name):
         """Run a the test for a single bioagent"""
-        logger.info("Running test on: %s." % ba_name)
-        self.start_bioagent(ba_name)
-        tm = Test_Module(self.input_files[ba_name], name='Test_' + ba_name)
-        tm.start()
-        self.stop_bioagent()
+        logger.info("RUNNING TEST ON: %s." % ba_name + 30*"=")
+        try:
+            self._start_bioagent(ba_name)
+            tm = Test_Module(self._input_files[ba_name], name='Test_' + ba_name)
+            tm.start()
+            self._stop_bioagent()
+        except:
+            logger.error('Encountered exception while running %s test.' % ba_name)
         return
 
-    def start_bioagent(self, ba_name):
+    def _start_bioagent(self, ba_name):
         """Start up the process for a bioagent"""
         logger.info("Starting bioagent thread for: %s." % ba_name)
-        if self.bioagent_handle is not None:
+        if self._bioagent_handle is not None:
             raise TestError('Attempted to start a bioagent with another already running.')
 
-        self.bioagent_handle = Bioagent_Thread(ba_name)
-        self.bioagent_handle.start()
-        if not self.bioagent_handle.is_alive():
+        self._bioagent_handle = Bioagent_Thread(ba_name)
+        self._bioagent_handle.start()
+        if not self._bioagent_handle.is_alive():
             raise TestError('Bioagent thread died unexpectedly.')
 
         return
 
-    def stop_bioagent(self):
+    def _stop_bioagent(self):
         """Stop the current bioagent"""
         logger.info("Stopping bioagent thread.")
-        if self.bioagent_handle is not None:
-            self.bioagent_handle.stop()
-            self.bioagent_handle.join(timeout=5)
-            if self.bioagent_handle.stopped():
-                self.bioagent_handle = None
+        if self._bioagent_handle is not None:
+            self._bioagent_handle.stop()
+            self._bioagent_handle.join(timeout=5)
+            if self._bioagent_handle.stopped():
+                self._bioagent_handle = None
             else:
                 raise TestError('Could not stop bioagent thread.')
         return
 
-
-    def stop_trips(self):
+    def _stop_trips(self):
         """Stop trips facilitator"""
         logger.info("Stopping trips facilitator.")
-        if self.trips_is_running():
-            self.trips_handle.kill()
-            time.sleep(3)
-            if self.trips_is_running():
+        if self._trips_is_running():
+            self._trips_handle.kill()
+            sleep(3)
+            if self._trips_is_running():
                 raise TestError('Could not kill trips.')
-            self.trips_handle = None
+            self._trips_handle = None
         return
 
     def run_tests(self):
         """Run all the tests"""
         logger.info("Running tests.")
-        self.start_trips()
-        for ba_name in self.run_with:
-            self.run_bioagent_test(ba_name)
-        self.stop_trips()
+        self._start_trips()
+        try:
+            for ba_name in self._test_list:
+                self._run_bioagent_test(ba_name)
+        finally:
+            self._stop_trips()
+            if self._cleaning:
+                self._cleanup()
         return
 
+    def _cleanup(self):
+        """Remove all new files spawned during test."""
+        for name in listdir(self._loc):
+            name_path = path.join(self._loc, name)
+            if name not in self._init_state and path.isfile(name_path):
+                remove(name_path)
 
 if __name__ == "__main__":
-    tester = Test_Harness(sys.argv[1:])
-    tester.run_tests()
+    try:
+        tester = Test_Harness(sys.argv[1:])
+        tester.run_tests()
+    except:
+        raise

ad331880709d1500f3de66cd8f04c139f91fea4a
Ben Gyori <ben.gyori@gmail.com>
2017-08-23 16:35:50 -0400

    Implement getting members and refactor

--- bioagents/biosense/biosense_module.py
+++ bioagents/biosense/biosense_module.py
@@ -6,6 +6,7 @@ logging.basicConfig(format='%(levelname)s: %(name)s - %(message)s',
 logger = logging.getLogger('BIOSENSE')
 import indra
 from indra.util import read_unicode_csv
+from indra.tools import expand_families
 from indra.sources import trips
 from bioagents import Bioagent
 from indra.databases import get_identifiers_url
@@ -49,28 +50,12 @@ class BioSense_Module(Bioagent):
     def respond_choose_sense(self, content):
         """Return response content to choose-sense request."""
         ekb = content.gets('ekb-term')
-        tp = trips.process_xml(ekb)
-        agents = get_agents(tp)
-        ambiguities = get_ambiguities(tp)
-        msg = KQMLPerformative('OK')
+        agents, ambiguities = process_ekb(ekb)
+        msg = KQMLPerformative('SUCCESS')
         if agents:
             kagents = []
             for term_id, agent_tuple in agents.items():
-                agent, ont_type, urls = agent_tuple
-                db_refs = '|'.join('%s:%s' % (k, v) for k, v in
-                                   agent.db_refs.items())
-                name = agent.name
-                kagent = KQMLList(term_id)
-                kagent.sets('name', agent.name)
-                kagent.sets('ids', db_refs)
-                url_parts = [KQMLList([':name', KQMLString(k),
-                                       ':dblink', KQMLString(v)])
-                             for k, v in urls.items()]
-                url_list = KQMLList()
-                for url_part in url_parts:
-                    url_list.append(url_part)
-                kagent.set('id-urls', url_list)
-                kagent.set('ont-type', ont_type)
+                kagent = get_kagent(agent_tuple, term_id)
                 kagents.append(kagent)
             msg.set('agents', KQMLList(kagents))
         if ambiguities:
@@ -81,11 +66,10 @@ class BioSense_Module(Bioagent):
     def respond_choose_sense_category(self, content):
         """Return response content to choose-sense-category request."""
         ekb = content.gets('ekb-term')
-        tp = trips.process_xml(ekb)
-        agent_dict = get_agents(tp)
-        if len(agent_dict) != 1:
+        agents, _ = process_ekb(ekb)
+        if len(agents) != 1:
             return make_failure('INVALID_AGENT')
-        agent = list(agent_dict.values())[0][0]
+        agent = list(agents.values())[0][0]
         category = content.gets('category')
         if category == 'kinase activity':
             msg = KQMLList('SUCCESS')
@@ -107,18 +91,16 @@ class BioSense_Module(Bioagent):
         """Return response content to choose-sense-is-member request."""
         # Get the member agent first
         ekb = content.gets('ekb-term')
-        tp = trips.process_xml(ekb)
-        agent_dict = get_agents(tp)
-        if len(agent_dict) != 1:
+        agents,_ = process_ekb(ekb)
+        if len(agents) != 1:
             return make_failure('INVALID_AGENT')
-        member_agent = list(agent_dict.values())[0][0]
+        member_agent = list(agents.values())[0][0]
         # Get the collection next
         ekb = content.gets('collection')
-        tp = trips.process_xml(ekb)
-        agent_dict = get_agents(tp)
-        if len(agent_dict) != 1:
-            return make_failure('INVALID_AGENT')
-        collection_agent = list(agent_dict.values())[0][0]
+        agents,_ = process_ekb(ekb)
+        if len(agents) != 1:
+            return make_failure('INVALID_COLLECTION')
+        collection_agent = list(agents.values())[0][0]
         if member_agent.isa(collection_agent, hierarchies):
             is_member = 'TRUE'
         else:
@@ -127,35 +109,64 @@ class BioSense_Module(Bioagent):
         msg.set('is-member', is_member)
         return msg
 
-def _read_kinases():
-    path = os.path.dirname(os.path.abspath(__file__))
-    kinase_table = read_unicode_csv(_indra_path + '/resources/kinases.tsv',
-                                    delimiter='\t')
-    gene_names = [lin[1] for lin in list(kinase_table)[1:]]
-    return gene_names
+    def respond_choose_sense_what_member(self, content):
+        """Return response content to choose-sense-what-member request."""
+        # Get the collection agent
+        ekb = content.gets('collection')
+        agents, _ = process_ekb(ekb)
+        if len(agents) != 1:
+            return make_failure('INVALID_COLLECTION')
 
+        term_id, (agent, ont_type, urls) = list(agents.items())[0]
+        members = get_members(agent)
+        if members is None:
+            return make_failure('COLLECTION_NOT_FAMILY_OR_COMPLEX')
 
-def _read_tfs():
-    path = os.path.dirname(os.path.abspath(__file__))
-    tf_table = \
-        read_unicode_csv(_indra_path + '/resources/transcription_factors.csv')
-    gene_names = [lin[1] for lin in list(tf_table)[1:]]
-    return gene_names
+        kagents = [get_kagent((agent, 'ONT::PROTEIN', get_urls(m))) for m in members]
 
-kinase_list = _read_kinases()
-tf_list = _read_tfs()
+        msg = KQMLList('SUCCESS')
+        msg.set('members', KQMLList(kagents))
+        return msg
 
-def get_agents(tp):
+def get_kagent(agent_tuple, term_id=None):
+    agent, ont_type, urls = agent_tuple
+    db_refs = '|'.join('%s:%s' % (k, v) for k, v in
+                       agent.db_refs.items())
+    name = agent.name
+    kagent = KQMLList(term_id) if term_id else KQMLList()
+    kagent.sets('name', agent.name)
+    kagent.sets('ids', db_refs)
+    url_parts = [KQMLList([':name', KQMLString(k),
+                           ':dblink', KQMLString(v)])
+                 for k, v in urls.items()]
+    url_list = KQMLList()
+    for url_part in url_parts:
+        url_list.append(url_part)
+    kagent.set('id-urls', url_list)
+    kagent.set('ont-type', ont_type)
+    return kagent
+
+def process_ekb(ekb):
+    tp = trips.process_xml(ekb)
+    agents = get_agent_tuples(tp)
+    ambiguities = get_ambiguities(tp)
+    return agents, ambiguities
+
+def get_agent_tuples(tp):
     terms = tp.tree.findall('TERM')
     all_agents = {}
     for term in terms:
         term_id = term.attrib['id']
         _, ont_type, _ = trips.processor._get_db_refs(term)
         agent = tp._get_agent_by_id(term_id, None)
-        urls = {k: get_identifiers_url(k, v) for k, v in agent.db_refs.items()
-                if k != 'TEXT'}
+        urls = get_urls(agent)
         all_agents[term_id] = (agent, ont_type, urls)
     return all_agents
+
+def get_urls(agent):
+    urls = {k: get_identifiers_url(k, v) for k, v in agent.db_refs.items()
+            if k != 'TEXT'}
+    return urls
 
 def get_ambiguities(tp):
     terms = tp.tree.findall('TERM')
@@ -166,6 +177,17 @@ def get_ambiguities(tp):
         if ambiguities:
             all_ambiguities[term_id] = ambiguities
     return all_ambiguities
+
+def get_members(agent):
+    dbname, dbid = agent.get_grounding()
+    if dbname != 'BE':
+        return None
+    eh = hierarchies['entity']
+    uri = eh.get_uri(dbname, dbid)
+    children_uris = eh.get_children(uri)
+    children_agents = [expand_families._agent_from_uri(uri)
+                       for uri in children_uris]
+    return children_agents
 
 def get_ambiguities_msg(ambiguities):
     sa = []
@@ -199,6 +221,25 @@ def make_failure(reason):
     msg = KQMLList('FAILURE')
     msg.set('reason', reason)
     return msg
+
+def _read_kinases():
+    path = os.path.dirname(os.path.abspath(__file__))
+    kinase_table = read_unicode_csv(_indra_path + '/resources/kinases.tsv',
+                                    delimiter='\t')
+    gene_names = [lin[1] for lin in list(kinase_table)[1:]]
+    return gene_names
+
+
+def _read_tfs():
+    path = os.path.dirname(os.path.abspath(__file__))
+    tf_table = \
+        read_unicode_csv(_indra_path + '/resources/transcription_factors.csv')
+    gene_names = [lin[1] for lin in list(tf_table)[1:]]
+    return gene_names
+
+kinase_list = _read_kinases()
+tf_list = _read_tfs()
+
 
 
 if __name__ == "__main__":

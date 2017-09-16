938955192702bf3e3bbc94e81b9ea3ba329863d3
Ben Gyori <ben.gyori@gmail.com>
2017-09-16 17:15:23 -0400

    Reformat MRA tests

--- tests/mra_test.py
+++ tests/mra_test.py
@@ -1,23 +1,10 @@
 import json
 from kqml import KQMLList
 from indra.statements import *
-from indra.sources.trips import trips_client
+from util import ekb_from_text
 from bioagents.mra import MRA, MRA_Module
 from bioagents.mra.mra_module import ekb_from_agent, get_target
 
-
-def test_build_model_from_ekb():
-    m = MRA()
-    html = trips_client.send_query('MAP2K1 phosphorylates ERK2.')
-    ekb_xml = trips_client.get_xml(html)
-    res = m.build_model_from_ekb(ekb_xml)
-    assert(res.get('model'))
-    assert(res.get('model_id') == 1)
-    assert(res.get('model_exec'))
-    assert(len(m.models[1]) == 1)
-    assert(isinstance(m.models[1][0], Phosphorylation))
-    assert(m.models[1][0].enz.name == 'MAP2K1')
-    assert(m.models[1][0].sub.name == 'MAPK1')
 
 def test_agent_to_ekb():
     egfr = Agent('EGFR', db_refs = {'HGNC': '3236', 'TEXT': 'EGFR'})
@@ -25,6 +12,64 @@ def test_agent_to_ekb():
     egfr_out = get_target(term)
     assert(isinstance(egfr_out, Agent))
     assert(egfr_out.name == 'EGFR')
+
+
+def test_build_model_from_ekb():
+    m = MRA()
+    ekb = ekb_from_text('MAP2K1 phosphorylates MAPK1.')
+    res = m.build_model_from_ekb(ekb)
+    assert(res.get('model'))
+    assert(res.get('model_id') == 1)
+    assert(res.get('model_exec'))
+    assert(len(m.models[1]) == 1)
+    assert(isinstance(m.models[1][0], Phosphorylation))
+    assert(m.models[1][0].enz.name == 'MAP2K1')
+
+
+def test_expand_model_from_ekb():
+    m = MRA()
+    ekb = ekb_from_text('MAP2K1 phosphorylates MAPK1.')
+    res = m.build_model_from_ekb(ekb)
+    model_id = res.get('model_id')
+    assert(res.get('model'))
+    assert(res.get('model_id') == 1)
+    assert(len(m.models[1]) == 1)
+    ekb = ekb_from_text('MAP2K2 phosphorylates MAPK1.')
+    res = m.expand_model_from_ekb(ekb, model_id)
+    assert(res.get('model'))
+    assert(res.get('model_id') == 2)
+    assert(len(m.models[2]) == 2)
+
+
+def test_respond_build_model_from_json():
+    mm = MRA_Module(testing=True)
+    st = Phosphorylation(Agent('MEK'), Agent('ERK'))
+    msg = KQMLList('BUILD-MODEL')
+    msg.sets('description', json.dumps(stmts_to_json([st])))
+    msg.sets('format', 'indra_json')
+    reply = mm.respond_build_model(msg)
+    assert(reply.get('model'))
+    assert(reply.get('model-id') == '1')
+
+
+def test_respond_expand_model_from_json():
+    mm = MRA_Module(testing=True)
+    st = Phosphorylation(Agent('MEK'), Agent('ERK'))
+    msg = KQMLList('BUILD-MODEL')
+    msg.sets('description', json.dumps(stmts_to_json([st])))
+    msg.sets('format', 'indra_json')
+    reply = mm.respond_build_model(msg)
+    assert(reply.get('model'))
+    assert(reply.get('model-id') == '1')
+    st = Phosphorylation(Agent('RAF'), Agent('MEK'))
+    msg = KQMLList('EXPAND-MODEL')
+    msg.sets('description', json.dumps(stmts_to_json([st])))
+    msg.sets('format', 'indra_json')
+    msg.set('model-id', '1')
+    reply = mm.respond_expand_model(msg)
+    assert(reply.get('model'))
+    assert(reply.get('model-id') == '2')
+
 
 def test_get_upstream():
     m = MRA()
@@ -35,41 +80,22 @@ def test_get_upstream():
     upstream = m.get_upstream(kras, model_id)
     assert(len(upstream) == 1)
     assert(upstream[0].name == 'EGFR')
+
+
+def test_respond_model_get_upstream():
     mm = MRA_Module(testing=True)
-    mm.mra = m
+    egfr = Agent('EGFR', db_refs = {'HGNC': '3236', 'TEXT': 'EGFR'})
+    kras = Agent('KRAS', db_refs = {'HGNC': '6407', 'TEXT': 'KRAS'})
+    stmts = [Activation(egfr, kras)]
+    model_id = mm.mra.new_model(stmts)
     kras_term = ekb_from_agent(kras)
     msg = KQMLList('MODEL-GET-UPSTREAM')
     msg.sets('target', kras_term)
     msg.set('model-id', str(model_id))
-    print(msg)
     reply = mm.respond_model_get_upstream(msg)
     ups = reply.get('upstream')
     assert(len(ups) == 1)
-    print(reply)
 
-def test_build_from_json():
-    mm = MRA_Module(testing=True)
-    st = Phosphorylation(Agent('MEK'), Agent('ERK'))
-    msg = KQMLList('BUILD-MODEL')
-    msg.sets('description', json.dumps(stmts_to_json([st])))
-    msg.sets('format', 'indra_json')
-    print(msg)
-    reply = mm.respond_build_model(msg)
-
-def test_expand_from_json():
-    mm = MRA_Module(testing=True)
-    st = Phosphorylation(Agent('MEK'), Agent('ERK'))
-    msg = KQMLList('BUILD-MODEL')
-    msg.sets('description', json.dumps(stmts_to_json([st])))
-    msg.sets('format', 'indra_json')
-    reply = mm.respond_build_model(msg)
-    st = Phosphorylation(Agent('RAF'), Agent('MEK'))
-    msg = KQMLList('EXPAND-MODEL')
-    msg.sets('description', json.dumps(stmts_to_json([st])))
-    msg.sets('format', 'indra_json')
-    msg.set('model-id', '1')
-    print(msg)
-    reply = mm.respond_expand_model(msg)
 
 def test_undo():
     mm = MRA_Module(testing=True)
@@ -82,6 +108,13 @@ def test_undo():
     kl = KQMLList.from_string('(MODEL-UNDO)')
     reply = mm.respond_model_undo(kl)
     print(reply)
+
+def test_has_mechanism():
+    m = MRA()
+    ekb = ekb_from_text('BRAF binds MEK')
+    m.build_model_from_ekb(ekb)
+    has_mechanism = m.has_mechanism(ekb, 1)
+    assert(has_mechanism)
 
 '''
 def test_replace_agent_one():
@@ -108,10 +141,3 @@ def test_find_family_members_id():
     assert(len(family_members) == 3)
     assert('BRAF' in family_members)
 '''
-
-def test_has_mechanism():
-    m = MRA()
-    ekb = '<ekb><EVENT id="V33716"><type>ONT::BIND</type><arg1 id="V33712" role=":AGENT" /><arg2 id="V33734" role=":AFFECTED" /></EVENT><TERM id="V33712" dbid="HGNC:1097|NCIT:C51194|NCIT:C17476"><features></features><type>ONT::GENE-PROTEIN</type><name>BRAF</name></TERM><TERM id="V33734" dbid="NCIT:C52823|NCIT:C105947|NCIT:C17808|HGNC:6840|UP:Q91447|UP:Q05116"><features></features><type>ONT::GENE-PROTEIN</type><name>MEK-1</name></TERM></ekb>'
-    m.build_model_from_ekb(ekb)
-    has_mechanism = m.has_mechanism(ekb, 1)
-    assert(has_mechanism)

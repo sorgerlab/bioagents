47265550be94e7a4cddcff7a09a6f702bb2ca67c
Ben Gyori <ben.gyori@gmail.com>
2017-09-16 22:58:59 -0400

    Add MRA module test for undo

--- tests/mra_test.py
+++ tests/mra_test.py
@@ -104,6 +104,19 @@ def test_respond_model_get_upstream():
     assert(len(ups) == 1)
 
 
+def test_respond_model_undo():
+    mm = MRA_Module(testing=True)
+    _, content = _get_build_model_request('HRAS activates RAF')
+    reply = mm.respond_build_model(content)
+    _, content = _get_expand_model_request('NRAS activates RAF', '1')
+    reply = mm.respond_expand_model(content)
+    content = KQMLList.from_string('(MODEL-UNDO)')
+    reply = mm.respond_model_undo(content)
+    assert reply.gets('model-id') == '3'
+    action = reply.get('action')
+    assert action is not None
+
+
 # #####################
 # MRA integration tests
 # #####################
@@ -112,6 +125,13 @@ def _get_build_model_request(text):
     content = KQMLList('BUILD-MODEL')
     descr = ekb_kstring_from_text(text)
     content.set('description', descr)
+    return _get_request(content), content
+
+def _get_expand_model_request(text, model_id):
+    content = KQMLList('EXPAND-MODEL')
+    descr = ekb_kstring_from_text(text)
+    content.set('description', descr)
+    content.set('model-id', model_id)
     return _get_request(content), content
 
 
@@ -185,18 +205,6 @@ class TestMissingDescriptionFailure(_FailureTest):
         print(content)
         return msg, content
 '''
-
-def test_undo():
-    mm = MRA_Module(testing=True)
-    kl = KQMLList.from_string('(BUILD-MODEL :DESCRIPTION "<ekb><EVENT id=\\"V34357\\"><type>ONT::ACTIVATE</type><arg1 id=\\"V34353\\" role=\\":AGENT\\" /><arg2 id=\\"V34364\\" role=\\":AFFECTED\\" /></EVENT><TERM id=\\"V34364\\" dbid=\\"FA:03114|BE:RAF|NCIT:C51274|UP:Q06891\\"><features></features><type>ONT::GENE-PROTEIN</type><name>RAF</name><drum-terms><drum-term dbid=\\"FA:03114\\" match-score=\\"1.0\\" name=\\"RAF subfamily\\" /><drum-term dbid=\\"BE:RAF\\" match-score=\\"1.0\\" name=\\"RAF\\" /><drum-term dbid=\\"NCIT:C51274\\" match-score=\\"0.82857\\" name=\\"RAF1\\" /><drum-term dbid=\\"UP:Q06891\\" match-score=\\"0.65714\\" name=\\"Trans-acting factor D\\" /></drum-terms></TERM><TERM id=\\"V34353\\" dbid=\\"NCIT:C52545|HGNC:5173|NCIT:C16659|NCIT:C17382\\"><features></features><type>ONT::GENE-PROTEIN</type><name>HRAS</name><drum-terms><drum-term dbid=\\"NCIT:C52545\\" match-score=\\"1.0\\" name=\\"HRAS\\" /><drum-term dbid=\\"HGNC:5173\\" match-score=\\"1.0\\" name=\\"Harvey rat sarcoma viral oncogene homolog\\" /><drum-term dbid=\\"NCIT:C16659\\" match-score=\\"0.82857\\" name=\\"oncogene H-ras\\" /><drum-term dbid=\\"NCIT:C17382\\" match-score=\\"0.82857\\" name=\\"p21 H-ras protein\\" /></drum-terms></TERM></ekb>")')
-    reply = mm.respond_build_model(kl)
-    print(reply)
-    kl = KQMLList.from_string('(EXPAND-MODEL :MODEL-ID 1 :DESCRIPTION "<ekb><EVENT id=\\"V34455\\"><type>ONT::ACTIVATE</type><arg1 id=\\"V34451\\" role=\\":AGENT\\" /><arg2 id=\\"V34462\\" role=\\":AFFECTED\\" /></EVENT><TERM id=\\"V34462\\" dbid=\\"FA:03114|BE:RAF|NCIT:C51274|UP:Q06891\\"><features></features><type>ONT::GENE-PROTEIN</type><name>RAF</name><drum-terms><drum-term dbid=\\"FA:03114\\" match-score=\\"1.0\\" name=\\"RAF subfamily\\" /><drum-term dbid=\\"BE:RAF\\" match-score=\\"1.0\\" name=\\"RAF\\" /><drum-term dbid=\\"NCIT:C51274\\" match-score=\\"0.82857\\" name=\\"RAF1\\" /><drum-term dbid=\\"UP:Q06891\\" match-score=\\"0.65714\\" name=\\"Trans-acting factor D\\" /></drum-terms></TERM><TERM id=\\"V34451\\" dbid=\\"NCIT:C52549|HGNC:7989|NCIT:C16889|NCIT:C17384\\"><features></features><type>ONT::GENE-PROTEIN</type><name>NRAS</name><drum-terms><drum-term dbid=\\"NCIT:C52549\\" match-score=\\"1.0\\" name=\\"NRAS\\" /><drum-term dbid=\\"HGNC:7989\\" match-score=\\"1.0\\" name=\\"neuroblastoma RAS viral oncogene homolog\\" /><drum-term dbid=\\"NCIT:C16889\\" match-score=\\"0.82857\\" name=\\"oncogene N-RAS\\" /><drum-term dbid=\\"NCIT:C17384\\" match-score=\\"0.82857\\" name=\\"p21 N-ras protein\\" /></drum-terms></TERM></ekb>")')
-    reply = mm.respond_expand_model(kl)
-    print(reply)
-    kl = KQMLList.from_string('(MODEL-UNDO)')
-    reply = mm.respond_model_undo(kl)
-    print(reply)
 
 def test_has_mechanism():
     m = MRA()

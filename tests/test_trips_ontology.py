1b673070e73b44d2c65039367d3d7cbe780cadc7
Ben Gyori <ben.gyori@gmail.com>
2017-10-03 11:46:01 -0400

    Test TRIPS ontology with disease names

---
+++ tests/test_trips_ontology.py
@@ -0,0 +1,9 @@
+from bioagents.resources.trips_ont_manager import trips_isa
+
+def test_isa_disease():
+    assert trips_isa('diabetes', 'disease')
+    assert not trips_isa('disease', 'diabetes')
+    assert trips_isa('cancer', 'medical-disorders-and-conditions')
+    assert trips_isa('ONT::CANCER', 'disease')
+    assert trips_isa('ont::cancer', 'disease')
+    assert trips_isa('CANCER', 'ONT::DISEASE')

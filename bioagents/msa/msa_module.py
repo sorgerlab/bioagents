b33a30bfb88cda9ccfbb548a746740552f3af37d
pagreene <patrick.anton.greene@gmail.com>
2017-10-12 17:30:31 -0400

    Implement basic msa response, passing tests.

--- bioagents/msa/msa_module.py
+++ bioagents/msa/msa_module.py
@@ -8,6 +8,7 @@ from indra.sources import trips
 from bioagents import Bioagent
 from indra.databases import get_identifiers_url
 from indra.preassembler.hierarchy_manager import hierarchies
+from indra.sources.trips.processor import TripsProcessor
 from kqml import KQMLModule, KQMLPerformative, KQMLList, KQMLString
 
 
@@ -35,12 +36,41 @@ class MSA_Module(Bioagent):
 
     def respond_phosphorylation_activating(self, content):
         """Return response content to phosphorylation_activating request."""
-        target = content.gets('target')
+        target_ekb = content.gets('target')
+        if target_ekb is None or target_ekb == '':
+            return self.make_failure('No target given.')
+        agent = self._get_agent(target_ekb)
+        logger.debug('Found agent (target): %s.' % agent.name)
         residue = content.gets('residue')
         position = content.gets('position')
+        related_results = [
+            s for s in self.signor_afs
+            if self._matching(s, agent, residue, position)
+            ]
+        if not len(related_results):
+            return self.make_failure(
+                "Could not find statement matching phosphorylation activating "
+                "%s %s %s." % (agent.name, residue, position)
+                )
         msg = KQMLPerformative('SUCCESS')
         msg.set('is-activating', 'TRUE')
         return msg
+
+    @staticmethod
+    def _get_agent(agent_ekb):
+        tp = TripsProcessor(agent_ekb)
+        terms = tp.tree.findall('TERM')
+        term_id = terms[0].attrib['id']
+        agent = tp._get_agent_by_id(term_id, None)
+        return agent
+
+    def _matching(self, stmt, agent, residue, position):
+        return (
+            stmt.agent.name == agent.name
+            and any([
+                (m.residue == residue and m.position == position)
+                for m in stmt.agent.mods
+                ]))
 
 
 if __name__ == "__main__":

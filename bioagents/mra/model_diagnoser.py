10284879501d6e36d02a2750b45b1a45fb79f449
Ben Gyori <ben.gyori@gmail.com>
2018-05-01 00:18:05 -0400

    Add initial model diagnoser

---
+++ bioagents/mra/model_diagnoser.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+from indra.mechlinker import MechLinker
+from indra.statements import *
+
+class ModelDiagnoser(object):
+    def __init__(self, statements):
+        self.statements = statements
+
+    def get_missing_activities(self):
+        ml = MechLinker(self.statements)
+        ml.gather_explicit_activities()
+        suggestions = []
+        for stmt in self.statements:
+            if isinstance(stmt, (Modification, RegulateActivity,
+                                 RegulateAmount)):
+                # The subj here is in an "active" position
+                subj, obj = stmt.agent_list()
+                subj_base = ml._get_base(subj)
+                # If it has any activities but isn't in an active state
+                # here
+                if subj_base.activity_types and not subj.activity:
+                    # We suggest making the subj active in this case
+                    suggestion = deepcopy(stmt)
+                    act_type = subj_base.activity_types[0]
+                    new_subj = deepcopy(subj)
+                    new_subj.activity = ActivityCondition(act_type, True)
+                    suggestion.set_agent_list([new_subj, obj])
+                    suggestions.append(suggestion)
+        return suggestions

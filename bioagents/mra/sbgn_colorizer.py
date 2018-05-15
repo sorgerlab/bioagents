ab9f7ca3a3469298a025d2737194a1e1485e3b7b
Daniel Milstein <djmilstein@gmail.com>
2018-05-15 07:28:23 -0400

    Flesh out colorized interface

--- bioagents/mra/sbgn_colorizer.py
+++ bioagents/mra/sbgn_colorizer.py
@@ -1,5 +1,9 @@
 from lxml import etree
 import os
+from collections import namedtuple
+
+# How a node should be colorized
+Style = namedtuple('Style', ['border_color', 'fill_color'])
 
 class SbgnColorizer(object):
     """Adds border color and fill colors to an SBGNviz diagram via extensions
@@ -7,27 +11,83 @@ class SbgnColorizer(object):
 
     Attributes
     ----------
-    tree : lxml.etree.Element
-        Root node for the XML tree of the current SBGN-ML document,
+    root : lxml.etree.Element
+        Root node for the XML tree of the original SBGN-ML document,
         incrementally modified with colors as functions of this object are
         called to do so
+    glyph_id_to_label : dict
+        Dictionary mapping the glyph id to the corresponding label for each
+        element in the original sbgn-ml
+    label_to_style : dict
+        For each node for which a non-default style is desired, a mapping
+        from the node's label to the node's style
     """
 
     def __init__(self, sbgn_text):
         """Initializes colorizes with uncolorized sbgn generated from a pysb
         model.
+
+        Parameters
+        ----------
+        sbgn_text: str
+            The XML text of the original uncolorized SBGN-ML document generated
+            from a pySB model
         """
+        self.label_to_style = {}
         self.sbgn_text = sbgn_text
-        self.tree = etree.fromstring(sbgn_text)
+        self.root = etree.fromstring(sbgn_text)
+
+        # Create a dictionary mapping glyph ids to the corresponding label
+        # More than one glyph might have the same label
+        self.glyph_id_to_label = {}
+        last_id = None
+        for element in self.root.iter():
+            if element.tag.endswith('glyph'):
+                last_id = element.get('id')
+            elif element.tag.endswith('label'):
+                if last_id is not None:
+                    label = element.get('text')
+                    if label is not None:
+                        self.glyph_id_to_label[last_id] = label
 
     def get_nodes(self):
         """Returns the labels of the nodes that can be colorized;
-        duplicate labels are possible."""
-        pass
+        duplicate labels are possible.
 
-    def set_colors(self, label, border_color, fill_color):
+        Returns
+        -------
+        labels: set<str>
+            The labels of the nodes in the diagram
+        """
+        return set(self.glyph_id_to_label.values())
+
+    def set_style(self, label, border_color, fill_color):
         """Colorizes all nodes with the specified label with the specified
-        border and fill color."""
+        border and fill color.
+
+        Parameters
+        ----------
+        label : str
+            Add a styel to nodes with this label
+        border_color : str
+            The border color, starting with # and followed by six hex digits
+        fill_color : str
+            The fill color, starting with # and followed by six hex digits
+        """
+        assert(border_color.startswith('#'))
+        assert(fill_color.startswith('#'))
+        self.label_to_style[label] = Style(border_color, fill_color)
+
+    def generate_xml(self):
+        """Generates XML that colorizes the nodes previously specified with
+        set_style.
+
+        Returns
+        -------
+        xml: str
+            SBGM-ML XML with extension notation understandable by SBGNviz
+            giving the colorign for the specified nodes
+        """
         pass
 
 if __name__ == '__main__':
@@ -38,3 +98,5 @@ if __name__ == '__main__':
         content = f.read()
 
     colorizer = SbgnColorizer(content)
+    print(colorizer.glyph_id_to_label)
+    colorizer.set_style('RAF', '#ff0000', '#00ff00')

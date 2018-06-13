9887e287004d36103f53385d610776b5971a844e
Ben Gyori <ben.gyori@gmail.com>
2018-06-13 00:10:44 -0400

    Include provenance for modeling

--- bioagents/mra/mra_module.py
+++ bioagents/mra/mra_module.py
@@ -87,8 +87,8 @@ class MRA_Module(Bioagent):
         msg.set('model-id', str(model_id))
         # Add the INDRA model json
         model = res.get('model')
-        #if model and (descr_format == 'ekb' or not descr_format):
-        #    self.send_background_support(model)
+        if model and (descr_format == 'ekb' or not descr_format):
+            self.send_background_support(model)
         model_msg = encode_indra_stmts(model)
         msg.sets('model', model_msg)
         # Add the diagrams
@@ -154,8 +154,8 @@ class MRA_Module(Bioagent):
                 content = KQMLList('SPOKEN')
                 content.sets('WHAT', explanation_str)
                 self.tell(content)
-        #if model_new and (descr_format == 'ekb' or not descr_format):
-        #    self.send_background_support(model_new)
+        if model_new and (descr_format == 'ekb' or not descr_format):
+            self.send_background_support(model_new)
         if model_new:
             model_new_msg = encode_indra_stmts(model_new)
             msg.sets('model-new', model_new_msg)

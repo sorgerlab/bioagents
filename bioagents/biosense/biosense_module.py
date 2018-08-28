74061f3d69e808cc04105b2cd0cbe1a48a515754
Albert Steppi <albert_steppi@hms.harvard.edu>
2018-08-28 17:11:08 -0400

    exactly didn't stage changes to biosense_module.py earlier

--- bioagents/biosense/biosense_module.py
+++ bioagents/biosense/biosense_module.py
@@ -6,6 +6,10 @@ from indra.util import read_unicode_csv
 from indra.tools import expand_families
 from indra.sources import trips
 from indra.sources.trips.processor import TripsProcessor
+from .biosense import BioSense, _get_urls
+from .biosense import InvalidAgentError, UnknownCategoryError
+from .biosense import InvalidCollectionError, CollectionNotFamilyOrComplexError
+
 from bioagents import Bioagent
 from indra.databases import get_identifiers_url, uniprot_client
 from indra.preassembler.hierarchy_manager import hierarchies
@@ -21,6 +25,11 @@ _indra_path = indra.__path__[0]
 
 
 class BioSense_Module(Bioagent):
+    def __init__(self, **kwargs):
+        # Instantiate a singleton BioSense agent
+        self.bs = BioSense()
+        super(BioSense_Module, self).__init__(**kwargs)
+        
     name = 'BioSense'
     tasks = ['CHOOSE-SENSE', 'CHOOSE-SENSE-CATEGORY',
              'CHOOSE-SENSE-IS-MEMBER', 'CHOOSE-SENSE-WHAT-MEMBER',
@@ -29,7 +38,8 @@ class BioSense_Module(Bioagent):
     def respond_choose_sense(self, content):
         """Return response content to choose-sense request."""
         ekb = content.gets('ekb-term')
-        agents, ambiguities = process_ekb(ekb)
+        groundings = self.bs.choose_sense(ekb)
+        agents, ambiguities = (groundings['agents'], groundings['ambiguities'])
         msg = KQMLPerformative('SUCCESS')
         if agents:
             kagents = []
@@ -45,110 +55,65 @@ class BioSense_Module(Bioagent):
     def respond_choose_sense_category(self, content):
         """Return response content to choose-sense-category request."""
         ekb = content.gets('ekb-term')
-        agents, _ = process_ekb(ekb)
-        if len(agents) != 1:
-            return make_failure('INVALID_AGENT')
-        agent = list(agents.values())[0][0]
         category = content.gets('category')
-        logger.info("Checking %s for category %s." % (agent, category))
-        reg_cat = category.lower().replace('-', ' ').replace('W::', '').replace('w::', '')
-        logger.info("Regularized category to \"%s\"." % reg_cat)
-        if reg_cat in ['kinase', 'kinase activity']:
-            msg = KQMLList('SUCCESS')
-            if agent.name in kinase_list:
-                msg.set('in-category', 'TRUE')
-            else:
-                msg.set('in-category', 'FALSE')
-        elif reg_cat == 'transcription factor':
-            msg = KQMLList('SUCCESS')
-            if agent.name in tf_list:
-                msg.set('in-category', 'TRUE')
-            else:
-                msg.set('in-category', 'FALSE')
-        elif reg_cat == 'phosphatase':
-            msg = KQMLList('SUCCESS')
-            if agent.name in phosphatase_list:
-                msg.set('in-category', 'TRUE')
-            else:
-                msg.set('in-category', 'FALSE')
-        elif reg_cat == 'enzyme':
-            msg = KQMLList('SUCCESS')
-            if agent.name in phosphatase_list or agent.name in kinase_list:
-                msg.set('in-category', 'TRUE')
-            else:
-                msg.set('in-category', 'FALSE')
-        else:
+        try:
+            in_category = self.bs.choose_sense_category(ekb, category)
+        except InvalidAgentError:
+            msg = make_failure('INVALID_AGENT')
+        except UnknownCategoryError:
             msg = make_failure('UNKNOWN_CATEGORY')
-            logger.info("Regularized category \"%s\" not recognized: options "
-                        "are %s." % (reg_cat, ['kinase', 'kinase activity',
-                                               'enzyme', 'transcription factor',
-                                               'phosphatase']))
+        else:
+            msg = KQMLList('SUCCESS')
+            msg.set('in-category',
+                    'TRUE' if in_category else 'FALSE')
         return msg
 
     def respond_choose_sense_is_member(self, content):
         """Return response content to choose-sense-is-member request."""
-        # Get the member agent first
-        ekb = content.gets('ekb-term')
-        agents, _ = process_ekb(ekb)
-        if len(agents) != 1:
-            return make_failure('INVALID_AGENT')
-        member_agent = list(agents.values())[0][0]
-        # Get the collection next
-        ekb = content.gets('collection')
-        agents, _ = process_ekb(ekb)
-        if len(agents) != 1:
-            return make_failure('INVALID_COLLECTION')
-        collection_agent = list(agents.values())[0][0]
-        if member_agent.isa(collection_agent, hierarchies):
-            is_member = 'TRUE'
+        agent_ekb = content.gets('ekb-term')
+        collection_ekb = content.gets('collection')
+        try:
+            is_member = self.bs.choose_sense_is_member(agent_ekb,
+                                                       collection_ekb)
+        except InvalidCollectionError:
+            msg = make_failure('INVALID_COLLECTION')
         else:
-            is_member = 'FALSE'
-        msg = KQMLList('SUCCESS')
-        msg.set('is-member', is_member)
+            msg = KQMLList('SUCCESS')
+            msg.set('is-member', 'TRUE' if is_member else 'FALSE')
         return msg
 
     def respond_choose_sense_what_member(self, content):
         """Return response content to choose-sense-what-member request."""
         # Get the collection agent
         ekb = content.gets('collection')
-        agents, _ = process_ekb(ekb)
-        if len(agents) != 1:
-            return make_failure('INVALID_COLLECTION')
-
-        term_id, (agent, ont_type, urls) = list(agents.items())[0]
-        members = get_members(agent)
-        if members is None:
-            return make_failure('COLLECTION_NOT_FAMILY_OR_COMPLEX')
-
-        kagents = [get_kagent((m, 'ONT::PROTEIN', get_urls(m)))
-                   for m in members]
-
-        msg = KQMLList('SUCCESS')
-        msg.set('members', KQMLList(kagents))
+        try:
+            members = self.bs.choose_sense_what_member(ekb)
+        except InvalidCollectionError:
+            msg = make_failure('INVALID_COLLECTION')
+        except CollectionNotFamilyOrComplexError:
+            msg = make_failure('COLLECTION_NOT_FAMILY_OR_COMPLEX')
+        else:
+            kagents = [get_kagent((m, 'ONT::PROTEIN', _get_urls(m)))
+                       for m in members]
+            msg = KQMLList('SUCCESS')
+            msg.set('members', KQMLList(kagents))
         return msg
 
     def respond_get_synonyms(self, content):
         """Respond to a query looking for synonyms of a protein."""
         ekb = content.gets('entity')
         try:
-            agent = self._get_agent(ekb)
-        except Exception as e:
-            return self.make_failure('INVALID_AGENT')
-        if agent is None:
-            return self.make_failure('INVALID_AGENT')
-
-        up_id = agent.db_refs.get('UP')
-        if not up_id:
-            return self.make_failure('INVALID_AGENT')
-
-        synonyms = uniprot_client.get_synonyms(up_id)
-        syns_kqml = KQMLList()
-        for s in synonyms:
-            entry = KQMLList()
-            entry.sets(':name', s)
-            syns_kqml.append(entry)
-        msg = KQMLList('SUCCESS')
-        msg.set('synonyms', syns_kqml)
+            synonyms = self.bs.get_synonyms(ekb)
+        except InvalidAgentError:
+            msg = self.make_failure('INVALID_AGENT')
+        else:
+            syns_kqml = KQMLList()
+            for s in synonyms:
+                entry = KQMLList()
+                entry.sets(':name', s)
+                syns_kqml.append(entry)
+            msg = KQMLList('SUCCESS')
+            msg.set('synonyms', syns_kqml)
         return msg
 
     @staticmethod
@@ -176,54 +141,6 @@ def get_kagent(agent_tuple, term_id=None):
     kagent.set('id-urls', url_list)
     kagent.set('ont-type', ont_type)
     return kagent
-
-
-def process_ekb(ekb):
-    tp = trips.process_xml(ekb)
-    agents = get_agent_tuples(tp)
-    ambiguities = get_ambiguities(tp)
-    return agents, ambiguities
-
-
-def get_agent_tuples(tp):
-    terms = tp.tree.findall('TERM')
-    all_agents = {}
-    for term in terms:
-        term_id = term.attrib['id']
-        _, ont_type, _ = trips.processor._get_db_refs(term)
-        agent = tp._get_agent_by_id(term_id, None)
-        urls = get_urls(agent)
-        all_agents[term_id] = (agent, ont_type, urls)
-    return all_agents
-
-
-def get_urls(agent):
-    urls = {k: get_identifiers_url(k, v) for k, v in agent.db_refs.items()
-            if k != 'TEXT'}
-    return urls
-
-
-def get_ambiguities(tp):
-    terms = tp.tree.findall('TERM')
-    all_ambiguities = {}
-    for term in terms:
-        term_id = term.attrib.get('id')
-        _, _, ambiguities = trips.processor._get_db_refs(term)
-        if ambiguities:
-            all_ambiguities[term_id] = ambiguities
-    return all_ambiguities
-
-
-def get_members(agent):
-    dbname, dbid = agent.get_grounding()
-    if dbname not in ['FPLX', 'BE']:
-        return None
-    eh = hierarchies['entity']
-    uri = eh.get_uri(dbname, dbid)
-    children_uris = sorted(eh.get_children(uri))
-    children_agents = [expand_families._agent_from_uri(uri)
-                       for uri in children_uris]
-    return children_agents
 
 
 def get_ambiguities_msg(ambiguities):
@@ -259,36 +176,6 @@ def make_failure(reason):
     msg = KQMLList('FAILURE')
     msg.set('reason', reason)
     return msg
-
-def _read_phosphatases():
-    path = os.path.dirname(os.path.abspath(__file__))
-    p_table = read_unicode_csv(_indra_path + 
-            '/resources/phosphatases.tsv', delimiter='\t')
-    # First column is phosphatase names
-    # Second column is HGNC ids
-    p_names = [row[0] for row in p_table]
-    return p_names
-
-
-def _read_kinases():
-    path = os.path.dirname(os.path.abspath(__file__))
-    kinase_table = read_unicode_csv(_indra_path + '/resources/kinases.tsv',
-                                    delimiter='\t')
-    gene_names = [lin[1] for lin in list(kinase_table)[1:]]
-    return gene_names
-
-
-def _read_tfs():
-    path = os.path.dirname(os.path.abspath(__file__))
-    tf_table = \
-        read_unicode_csv(_indra_path + '/resources/transcription_factors.csv')
-    gene_names = [lin[1] for lin in list(tf_table)[1:]]
-    return gene_names
-
-
-kinase_list = _read_kinases()
-tf_list = _read_tfs()
-phosphatase_list = _read_phosphatases()
 
 
 if __name__ == "__main__":

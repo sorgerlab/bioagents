91125fda1749639490675f86bab52a202d300fcb
Ben Gyori <ben.gyori@gmail.com>
2018-09-04 02:13:42 -0400

    Update MSA to query with FPLX and update test cache

--- bioagents/msa/msa_module.py
+++ bioagents/msa/msa_module.py
@@ -85,9 +85,6 @@ class MSA_Module(Bioagent):
         logger.info("Looking for statements with agent %s of type %s."
                     % (str(agent), 'ActiveForm'))
         for namespace, name in agent.db_refs.items():
-            # TODO: Remove this eventually, as it is a temporary work-around.
-            if namespace == 'FPLX':
-                namespace = 'BE'
             logger.info("Checking namespace: %s" % namespace)
             stmts = get_statements(agents=['%s@%s' % (name, namespace)],
                                    stmt_type='ActiveForm')

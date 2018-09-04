d562624d9043868b761f294cf08cfffdf7cd9cf6
Ben Gyori <ben.gyori@gmail.com>
2018-09-04 02:31:12 -0400

    Use a working PMID in MSA test

--- bioagents/tests/msa_test.py
+++ bioagents/tests/msa_test.py
@@ -156,7 +156,7 @@ class TestMsaPaperGraph(_IntegrationTest):
 
     def create_message(self):
         content = KQMLList('GET-PAPER-MODEL')
-        content.set('pmid', 'PMID-8436299')
+        content.set('pmid', 'PMID-27906130')
         return get_request(content), content
 
     def check_response_to_message(self, output):

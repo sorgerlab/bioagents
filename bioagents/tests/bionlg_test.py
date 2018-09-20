6b6fe99691836be0f59f9863ed55dadf38de7804
pagreene <patrick.anton.greene@gmail.com>
2018-09-20 13:21:20 -0400

    Create integration test to replace more primitive test.

--- bioagents/tests/bionlg_test.py
+++ bioagents/tests/bionlg_test.py
@@ -1,11 +1,47 @@
-from kqml import KQMLList
-from bioagents.bionlg.bionlg_module import BioNLG_Module
+import json
 
-def test_active_flag():
-    kp = KQMLList.from_string('(INDRA-TO-NL :STATEMENTS "[{\\"obj\\": {\\"db_refs\\": {\\"TEXT\\": \\"MAP-2-K-1\\", \\"HGNC\\": \\"6840\\", \\"UP\\": \\"Q02750\\", \\"NCIT\\": \\"C17808\\"}, \\"name\\": \\"MAP2K1\\"}, \\"type\\": \\"Activation\\", \\"obj_activity\\": \\"activity\\", \\"evidence\\": [{\\"epistemics\\": {\\"section_type\\": null}, \\"source_api\\": \\"trips\\"}], \\"subj\\": {\\"activity\\": {\\"is_active\\": true, \\"activity_type\\": \\"activity\\"}, \\"db_refs\\": {\\"TEXT\\": \\"BRAF\\", \\"HGNC\\": \\"1097\\", \\"UP\\": \\"P15056\\", \\"NCIT\\": \\"C17476\\"}, \\"name\\": \\"BRAF\\"}, \\"id\\": \\"863fec09-025b-4e9b-8863-16e2601741d2\\"}]")')
-    bn = BioNLG_Module(testing=True)
-    res = bn.respond_indra_to_nl(kp)
-    nl = res.get('NL')
-    assert(len(nl) == 1)
-    sentence = nl[0].string_value()
-    assert(sentence == 'Active BRAF activates MAP2K1')
+from kqml import KQMLList
+
+from indra.statements import Agent, Activation, BoundCondition, stmts_to_json, \
+    ActivityCondition
+
+from bioagents.tests.util import get_request
+from bioagents.bionlg.bionlg_module import BioNLG_Module
+from bioagents.tests.integration import _IntegrationTest
+
+
+kras = Agent('KRAS')
+braf = Agent('BRAF')
+map2k1 = Agent('MAP2K1')
+gtp = Agent('GTP')
+active_braf = Agent('BRAF', activity=ActivityCondition('activity', True))
+kras_bound = Agent("KRAS", bound_conditions=[BoundCondition(gtp, True)])
+
+
+class _NlgTestBase(_IntegrationTest):
+    message_funcs = ['statements']
+
+    # Define these in the sub-classes with statements, and sentences that
+    # correspond to those statements.
+    statements = []
+    sentences = []
+
+    def __init__(self, *args, **kwargs):
+        super(_NlgTestBase, self).__init__(BioNLG_Module)
+
+    def create_statements(self):
+        content = KQMLList('INDRA-TO-NL')
+        content.sets('statements', json.dumps(stmts_to_json(self.statements)))
+        return get_request(content), content
+
+    def check_response_to_statements(self, output):
+        assert output.head() == 'OK', output
+        nl_list = output.get('nl')
+        for i, sent in enumerate(nl_list):
+            assert sent.string_value() == self.sentences[i],\
+                "Expected %s, but got %s." % (sent, self.sentences[i])
+
+
+class TestActiveFlag(_NlgTestBase):
+    statements = [Activation(active_braf, map2k1)]
+    sentences = ["Active BRAF activates MAP2K1"]
